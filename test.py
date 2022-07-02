6dbf3863f1f66ca618ea9b9dd700350b6d7d02a4
Narek <meloyann87@gmail.com>
2022-07-02 11:54:57 +0400

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import torch
 if torch.cuda.is_available():
     device = torch.device('cuda')
     print("I will use GPU: ", torch.cuda.get_device_name(0))
+#Just testing with .py script if my CUDA instalation was successfull:)
